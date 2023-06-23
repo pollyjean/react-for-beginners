cc9f24fa730a9e920cd4960d9f2ec97cbca2b32e
pollyjean <pollyjean@naver.com>
2023-06-23 19:39:02 +0900

    react router v6 part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [dollars, setDollars] = useState(0);
-  const [selected, setSelected] = useState(0);
-  const [coinSymbol, setCoinSymbol] = useState("");
-  const selectForm = document.querySelector("select");
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setDollars(value);
-  };
-  const onSelect = () => {
-    setSelected("");
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const option = selectForm.querySelector("option:checked");
-    setSelected(selectForm.value);
-    setCoinSymbol(option.getAttribute("data-symbol"));
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year')).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log("error : ", error);
-      });
+    getMovies();
   }, []);
-
   return (
-    <div>
-      <h1>코인 변환기 {loading ? "" : `(코인 종류 ${coins.length}개)`}</h1>
-      {loading ? (
-        <strong>loading...</strong>
-      ) : (
-        <>
-          <form onSubmit={onSubmit}>
-            <select onChange={onSelect}>
-              {coins.map((item) => (
-                <option key={item.id} value={item.quotes.USD.price} data-symbol={item.symbol}>
-                  [{item.symbol}]{item.name} : {item.quotes.USD.price} USD
-                </option>
-              ))}
-            </select>
-            <hr />
-            <label htmlFor="dollar">Dollar </label>
-            <input id="dollar" type="text" value={dollars} onChange={onChange} maxLength="20" />
-            <button type="submit">Exchange to Coin</button>
-          </form>
-          <p>{selected ? (!dollars ? "" : `${dollars / selected} ${coinSymbol}`) : ""}</p>
-        </>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home loading={loading} movies={movies} />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/Movie.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Movie = ({ coverImg, coverImg2x, title, summary, year, genres }) => {
+  return (
+    <li>
+      <picture>
+        <source srcSet={coverImg} media="(max-width:720px)" />
+        <img src={coverImg2x} alt={`${title}'s poster`} />
+      </picture>
+      <h2>
+        <Link to={'/movie'}>
+          {title} {`(${year})`}
+        </Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre, index) => (
+          <li key={index}>{genre}</li>
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+Movie.propTypes = {
+  coverImg: PropTypes.string,
+  coverImg2x: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
