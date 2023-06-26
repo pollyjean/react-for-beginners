9d8da83c6941d425e58958a2a62c1d906693cd4b
pollyjean <pollyjean@naver.com>
2023-06-26 17:40:07 +0900

    css 없이 기능 완성

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,5 +1,30 @@
-const Detail = () => {
-  return <h1>Detail</h1>;
+import { useParams } from "react-router-dom";
+
+const Detail = ({ loading, movies }) => {
+  const { id } = useParams();
+  const movie = movies.filter((item) => {
+    return item.id === Number(id);
+  })[0];
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <img src={movie.large_cover_image} alt={`${movie.title}'s poster`} />
+          <h2>
+            {movie.title} {`(${movie.year})`}
+          </h2>
+          <p>{movie.summary}</p>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Detail;
