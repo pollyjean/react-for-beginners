3212b49f05af25dd73f8bcf759d7da0deb2fab3b
pollyjean <pollyjean@naver.com>
2023-06-26 19:11:33 +0900

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home loading={loading} movies={movies} />} />
-        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail loading={loading} movies={movies} />} />
+        <Route path="/" element={<Home loading={loading} movies={movies} />} />
+        <Route path="/movie/:id" element={<Detail loading={loading} movies={movies} />} />
       </Routes>
     </Router>
   );
